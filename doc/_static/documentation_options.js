ed2591246591a49c10d35eca9a381766ec23ed6f
abhisheksinghtomar <54301487+abhisheksinghtomar@users.noreply.github.com>
2024-05-31 08:51:43 +0000

    Deploying to gh-pages from @ abhisheksinghtomar/skiboot@326a4666bd69ca071c4f7d2eacf868b11099baee 🚀

--- doc/_static/documentation_options.js
+++ doc/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '9104584',
+    VERSION: '326a466',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
